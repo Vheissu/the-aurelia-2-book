e447d5104d1dcf0f900825018027abb75e00a321
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-13 20:20:20 +1000

    feat(chapter-3): push state routing enabled

--- chapters/chapter3/src/main.ts
+++ chapters/chapter3/src/main.ts
@@ -7,9 +7,6 @@ Aurelia
     // optionally add the shared styles for all components
     sharedStyles: [shared]
   }))
-  .register(RouterConfiguration)
-  // To use HTML5 pushState routes, replace previous line with the following
-  // customized router config.
-  // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
+  .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
