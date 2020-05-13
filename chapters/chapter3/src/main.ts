64cda61b1049650fe20e5e41843aa614dda65fee
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-13 20:16:15 +1000

    feat(app): import shared styles

--- chapters/chapter3/src/main.ts
+++ chapters/chapter3/src/main.ts
@@ -1,17 +1,12 @@
-import Aurelia, { RouterConfiguration/*, StyleConfiguration*/ } from 'aurelia';
+import Aurelia, { RouterConfiguration, StyleConfiguration } from 'aurelia';
 import { MyApp } from './my-app';
-// Css files imported in this main file are NOT processed by style-loader
-// They are for sharedStyles in shadowDOM.
-// However, css files imported in other js/ts files are processed by style-loader.
-// import shared from './shared.css';
+import shared from './shared-styles.css';
 
 Aurelia
-  /*
   .register(StyleConfiguration.shadowDOM({
     // optionally add the shared styles for all components
     sharedStyles: [shared]
   }))
-  */
   .register(RouterConfiguration)
   // To use HTML5 pushState routes, replace previous line with the following
   // customized router config.
