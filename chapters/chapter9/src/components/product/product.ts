79ba14b908a6decdf2609b062b33a9217400608a
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-18 15:19:03 +1000

    feat(chapter9): product component styling

--- chapters/chapter9/src/components/product/product.ts
+++ chapters/chapter9/src/components/product/product.ts
@@ -2,4 +2,12 @@ import { bindable } from 'aurelia';
 
 export class Product {
     @bindable private product;
+
+    private addToCart(): void {
+        const existingCart = JSON.parse(localStorage.getItem('cart')) ?? [];
+
+        existingCart.push(this.product);
+
+        localStorage.setItem('cart', JSON.stringify(existingCart));
+    }
 }
