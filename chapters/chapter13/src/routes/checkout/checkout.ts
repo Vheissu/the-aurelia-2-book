0d7e3499e40879d63bbf3a91b10dfeb84efb4227
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-20 22:18:51 +1000

    feat(chapter13): checkout validation

--- chapters/chapter13/src/routes/checkout/checkout.ts
+++ chapters/chapter13/src/routes/checkout/checkout.ts
@@ -3,17 +3,63 @@ import { IRouteableComponent } from '@aurelia/router';
 import { inject } from 'aurelia';
 import { newInstanceForScope } from '@aurelia/kernel';
 
-import { IValidationRules } from '@aurelia/validation';
+import { IValidationRules, ValidationRules } from '@aurelia/validation';
 import { IValidationController } from '@aurelia/validation-html';
 
 @inject(ApiService)
 export class Checkout implements IRouteableComponent {
+    private details = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        address: '',
+        address2: '',
+        country: '',
+        state: '',
+        zip: '',
+        paymentType: '',
+        ccName: '',
+        ccNumber: '',
+        ccExpiration: '',
+        ccCvv: ''
+    };
+
     private cart = [];
     private total;
     private totalItems = 0;
 
-    constructor(private api: ApiService, @newInstanceForScope(IValidationController) private validationController: IValidationController, ) {
-
+    constructor(private api: ApiService, @newInstanceForScope(IValidationController) private validationController: IValidationController, @IValidationRules validationRules: IValidationRules) {
+        validationRules
+            .on(this.details)
+            .ensure('firstName')
+                .required()
+            .ensure('lastName')
+                .required()
+            .ensure('email')
+                .email()
+                .required()
+            .ensure('address')
+                .required()
+            .ensure('country')
+                .required()
+            .ensure('state')
+                .required()
+            .ensure('zip')
+                .required()
+            .ensure('paymentType')
+                .required()
+            .ensure('ccName')
+                .required()
+                .when((p => p.paymentType === 'credit'))
+            .ensure('ccNumber')
+                .required()
+                .when((p => p.paymentType === 'credit'))
+            .ensure('ccExpiration')
+                .required()
+                .when((p => p.paymentType === 'credit'))
+            .ensure('ccCvv')
+                .required()
+                .when((p => p.paymentType === 'credit'))
     }
 
     public beforeBind(): void {
