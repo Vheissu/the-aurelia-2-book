02f9902c70595fcdd5cce02da9449677c906c0b9
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-21 15:56:05 +1000

    feat(chapter14): login screen

--- chapters/chapter14/src/routes/auth/login.ts
+++ chapters/chapter14/src/routes/auth/login.ts
@@ -1,5 +1,5 @@
 import { IRouteableComponent } from '@aurelia/router';
-import { ApiService } from './../../services/api-service';
+import { ApiService } from '../../services/api-service';
 import { IRouter } from 'aurelia';
 
 export class Login implements IRouteableComponent {
@@ -8,5 +8,13 @@ export class Login implements IRouteableComponent {
 
     constructor(@IRouter private router: IRouter, private auth: ApiService) {
 
+    }
+
+    async submit(): Promise<void> {
+        if (this.username && this.password) {
+            const login = await this.auth.login(this.username, this.password);
+
+            console.log(login);
+        }
     }
 }
