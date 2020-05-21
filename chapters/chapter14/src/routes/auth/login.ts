5e5491481fffe2eab0edb9f0faffb1dc529ab911
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-21 16:43:01 +1000

    fix(chapter14): erroneous service imports

--- chapters/chapter14/src/routes/auth/login.ts
+++ chapters/chapter14/src/routes/auth/login.ts
@@ -1,23 +1,21 @@
+import { AuthService } from './../../services/auth-service';
 import { IRouteableComponent } from '@aurelia/router';
-import { ApiService } from '../../services/api-service';
 import { IRouter } from 'aurelia';
 
 export class Login implements IRouteableComponent {
     private username;
     private password;
 
-    constructor(@IRouter private router: IRouter, private auth: ApiService) {
+    constructor(@IRouter private router: IRouter, private auth: AuthService) {
 
     }
 
     async submit(): Promise<void> {
         if (this.username && this.password) {
             try {
-                const login = await this.auth.login(this.username, this.password);
+                await this.auth.login(this.username, this.password);
 
-                if (login.username) {
-                    this.router.goto('/dashboard');
-                }
+                this.router.goto('/dashboard');
             } catch (e) {
                 window.alert('There was an error.')
             }
--- chapters/chapter14/src/routes/auth/register.ts
+++ chapters/chapter14/src/routes/auth/register.ts
@@ -1,5 +1,5 @@
+import { AuthService } from './../../services/auth-service';
 import { IRouteableComponent } from '@aurelia/router';
-import { ApiService } from '../../services/api-service';
 import { IRouter } from 'aurelia';
 
 export class Register implements IRouteableComponent {
@@ -7,18 +7,16 @@ export class Register implements IRouteableComponent {
     private password;
     private password2;
 
-    constructor(@IRouter private router: IRouter, private auth: ApiService) {
+    constructor(@IRouter private router: IRouter, private auth: AuthService) {
 
     }
 
     async submit(): Promise<void> {
         if (this.username && this.password && this.password2 && this.password === this.password2) {
             try {
-                const register = await this.auth.register(this.username, this.password);
+                await this.auth.register(this.username, this.password);
 
-                if (register.success) {
-                    this.router.goto('/dashboard');
-                }
+                this.router.goto('/dashboard');
             } catch (e) {
                 window.alert('There was an error registering.')
             }
