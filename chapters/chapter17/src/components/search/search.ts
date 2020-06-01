b21b158335c5c0b87745e15c717ee9619dfbb2e0
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-06-01 15:08:32 +1000

    feat(chapter17): tests

---
+++ chapters/chapter17/src/components/search/search.ts
@@ -0,0 +1,35 @@
+import { ApiService } from '../../services/api-service';
+import { ICustomElementViewModel, BindingMode, containerless } from '@aurelia/runtime';
+import { inject, EventAggregator, bindable } from 'aurelia';
+
+@inject(ApiService, EventAggregator, Element)
+@containerless()
+export class Search implements ICustomElementViewModel {
+    private results = [];
+    private searchValue = '';
+
+    @bindable({ mode: BindingMode.twoWay }) private showing = false;
+
+    constructor(private api: ApiService, private ea: EventAggregator, private element: Element) {
+
+    }
+
+    keypress(event: KeyboardEvent) {
+        if (event.key === 'Escape') {
+            this.showing = false;
+            return false;
+        }
+
+        return true;
+    }
+
+    async search() {
+        if (this.searchValue.length >= 3) {
+            this.results = await this.api.search(this.searchValue);
+        }
+    }
+
+    resultClick(): void {
+        this.showing = false;
+    }
+}
---
+++ chapters/chapter17/test/api-service.spec.ts
@@ -0,0 +1,54 @@
+import { ApiService } from '../src/services/api-service';
+import { HttpClient, EventAggregator, DOM } from 'aurelia';
+
+import { enableFetchMocks } from 'jest-fetch-mock';
+enableFetchMocks();
+
+describe('API Service', () => {
+    let sut: ApiService;
+    let ea: EventAggregator;
+    let http: HttpClient;
+
+    beforeEach(() => {
+        ea = new EventAggregator();
+        http = new HttpClient(DOM);
+
+        sut = new ApiService(http, ea);
+
+        localStorage.clear();
+        fetchMock.resetMocks();
+    });
+
+    test('Customer has two items in their cart, return an array of products', () => {
+        localStorage.setItem('cart', JSON.stringify([{id: 1234, title: 'test', quantity: 1}, { id: 342342, title: 'another test', quantity: 1 }]));
+
+        expect(sut.getCart()).toHaveLength(2);
+    });
+
+    test('Customer has nothing in their cart, return an empty array', () => {
+        expect(sut.getCart()).toHaveLength(0);
+    });
+
+    test('Customer has two items, and a total of six products', () => {
+        localStorage.setItem('cart', JSON.stringify([{id: 1234, title: 'test', quantity: 3}, { id: 342342, title: 'another test', quantity: 3 }]));
+
+        expect(sut.getCartTotal()).toStrictEqual(6);
+    });
+
+    test('API returns two search results from the server', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify([{ id: 1222, title: 'Some Product' }, { id: 392, title: 'Another Product' }]));
+
+        const results = await sut.search('test term');
+
+        expect(results).toHaveLength(2);
+        expect(results[0].id).toStrictEqual(1222);
+    });
+
+    test('API threw an error trying to perform search', async () => {
+        fetchMock.mockReject(new Error('server had an error'));
+
+        const results = await sut.search('test term');
+
+        console.log(results);
+    });
+});
