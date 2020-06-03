be29a53205ebffb5ba9299118776b536c98519b5
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-06-03 22:50:08 +1000

    feat(chapter17): search test case

---
+++ chapters/chapter17/test/search.spec.ts
@@ -0,0 +1,52 @@
+import { TestContext, TestConfiguration } from '@aurelia/testing';
+import { CustomElement } from '@aurelia/runtime';
+import Aurelia from 'aurelia';
+import { Search } from './../src/components/search/search';
+
+import { enableFetchMocks } from 'jest-fetch-mock';
+enableFetchMocks();
+
+describe('Search', () => {
+    it('should render', async () => {
+        const ctx = TestContext.createHTMLTestContext();
+
+        const host = ctx.createElement('div');
+
+        const viewModel = class Host {
+        }
+
+        const component = CustomElement.define({ name: 'app', template: `<search></search>` }, viewModel);
+        const au = new Aurelia(ctx.container).register(TestConfiguration, Search).app({ host, component });
+
+        await au.start().wait();
+
+        const searchInput: HTMLInputElement = host.querySelector('input[type="search"]');
+
+        expect(searchInput).not.toBeUndefined();
+
+        await au.stop().wait();
+    });
+
+    it('display search results', async () => {
+        const ctx = TestContext.createHTMLTestContext();
+
+        const host = ctx.createElement('div');
+
+        const viewModel = class Host {
+            showSearch = true;
+            searchValue = 'test value';
+            search;
+        }
+
+        const component = CustomElement.define({ name: 'app', template: `<search showing.bind="showSearch" view-model.ref="search></search>` }, viewModel);
+        const au = new Aurelia(ctx.container).register(TestConfiguration, Search).app({ host, component });
+
+        await au.start().wait();
+
+        fetchMock.mockResponseOnce(JSON.stringify([{ id: 1222, title: 'Some Product' }, { id: 392, title: 'Another Product' }]));
+
+        console.log(viewModel.search);
+
+        await au.stop().wait();
+    });
+});
