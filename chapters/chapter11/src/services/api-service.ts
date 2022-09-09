02d95b9cf3491f7eca60d03c22089f6a501ce18e
Dwayne <dwaynecharrington@gmail.com>
2022-09-09 15:49:21 +1000

    feat(chapter11): updating chapter 11

--- chapters/chapter11/src/services/api-service.ts
+++ chapters/chapter11/src/services/api-service.ts
@@ -1,9 +1,12 @@
-import { HttpClient, json } from '@aurelia/fetch-client';
-import { inject } from 'aurelia';
+import { DI } from 'aurelia';
+import { IHttpClient, json } from '@aurelia/fetch-client';
 
-@inject(HttpClient)
+export const IApiService = DI.createInterface<IApiService>("IApiService", x => x.singleton(ApiService));
+
+export interface IApiService extends ApiService {  }
+
 export class ApiService {
-    constructor(private http: HttpClient) {
+    constructor(@IHttpClient private http: IHttpClient) {
         // Call the configure method to get the configuration object
         http.configure((config) => {
             // Prefix all API requests with this URL, it saves us having to repeat it
@@ -37,21 +40,24 @@ export class ApiService {
 
     // A login method to verify a users login credentials
     async login(username: string, password: string): Promise<any> {
-        const response = await this.http.post('/user', json({
-            username,
-            password
-        }));
+        const response = await this.http.post('/user', json({username, password}));
+
+        return response.json();
+    }
+
+    async register(username: string, password: string): Promise<any> {
+        const response = await this.http.post('/register', json({ username, password}));
 
         return response.json();
     }
 
     addToCart(product: any): any[] {
         const existingCart = JSON.parse(localStorage.getItem('cart')) ?? [];
-
+    
         existingCart.push(product);
-
+    
         localStorage.setItem('cart', JSON.stringify(existingCart));
-
+    
         return existingCart;
     }
 }
