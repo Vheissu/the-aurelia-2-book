22c4cf4d942e319806afdc881b53865960ab8f9e
Dwayne <dwaynecharrington@gmail.com>
2022-09-09 19:45:06 +1000

    fix(chapter13): missing methods and imports

--- chapters/chapter13/src/routes/store-checkout/store-checkout.ts
+++ chapters/chapter13/src/routes/store-checkout/store-checkout.ts
@@ -1,103 +1,114 @@
-import { ApiService, IApiService } from '../../services/api-service';
-import { IRouteableComponent, IRouter } from '@aurelia/router';
+import { IApiService } from "../../services/api-service";
+import { IRouteableComponent } from "@aurelia/router";
 
-import { newInstanceForScope } from '@aurelia/kernel';
-import { IValidationRules } from '@aurelia/validation';
-import { IValidationController, ValidationResultPresenterService } from '@aurelia/validation-html';
+import { newInstanceForScope } from "@aurelia/kernel";
+import {
+  IValidationController,
+  IValidationResultPresenterService,
+} from "@aurelia/validation-html";
+import { IValidationRules } from "@aurelia/validation";
+import { IRouter } from "aurelia";
 
 const sleep = (ms: number) => setTimeout(() => Promise.resolve(), ms);
 
 export class StoreCheckout implements IRouteableComponent {
-    private details = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        address: '',
-        address2: '',
-        country: '',
-        state: '',
-        zip: '',
-        paymentType: 'credit',
-        ccName: '',
-        ccNumber: '',
-        ccExpiration: '',
-        ccCvv: ''
-    };
+  private details = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    address2: "",
+    country: "",
+    state: "",
+    zip: "",
+    paymentType: "credit",
+    ccName: "",
+    ccNumber: "",
+    ccExpiration: "",
+    ccCvv: "",
+  };
 
-    private processing = false;
+  private processing = false;
 
-    private presenter: ValidationResultPresenterService;
+  private cart = [];
+  private total;
+  private totalItems = 0;
 
-    private cart = [];
-    private total;
-    private totalItems = 0;
+  constructor(
+    @IApiService private api: IApiService,
+    @IRouter private router: IRouter,
+    @newInstanceForScope(IValidationController)
+    private validationController: IValidationController,
+    @IValidationRules validationRules: IValidationRules,
+    @IValidationResultPresenterService private readonly presenter: IValidationResultPresenterService
+  ) {
+    this.validationController.addSubscriber(this.presenter);
 
-    constructor(@IApiService private api: ApiService, @IRouter private router: IRouter, @newInstanceForScope(IValidationController) private validationController: IValidationController, @IValidationRules validationRules: IValidationRules) {
-        this.presenter = new ValidationResultPresenterService();
-        this.validationController.addSubscriber(this.presenter);
-        
-        validationRules
-            .on(this.details)
-            .ensure('firstName')
-                .required()
-            .ensure('lastName')
-                .required()
-            .ensure('email')
-                .required()
-                .email()
-            .ensure('address')
-                .required()
-            .ensure('country')
-                .required()
-            .ensure('state')
-                .required()
-            .ensure('zip')
-                .required()
-            .ensure('paymentType')
-                .required()
-            .ensure('ccName')
-                .required()
-                .when((p => p.paymentType === 'credit'))
-            .ensure('ccNumber')
-                .required()
-                .when((p => p.paymentType === 'credit'))
-            .ensure('ccExpiration')
-                .required()
-                .when((p => p.paymentType === 'credit'))
-            .ensure('ccCvv')
-                .required()
-                .when((p => p.paymentType === 'credit'))
+    validationRules
+      .on(this.details)
+      .ensure("firstName")
+      .required()
+      .ensure("lastName")
+      .required()
+      .ensure("email")
+      .required()
+      .email()
+      .ensure("address")
+      .required()
+      .ensure("country")
+      .required()
+      .ensure("state")
+      .required()
+      .ensure("zip")
+      .required()
+      .ensure("paymentType")
+      .required()
+      .ensure("ccName")
+      .required()
+      .when((p) => p.paymentType === "credit")
+      .ensure("ccNumber")
+      .required()
+      .when((p) => p.paymentType === "credit")
+      .ensure("ccExpiration")
+      .required()
+      .when((p) => p.paymentType === "credit")
+      .ensure("ccCvv")
+      .required()
+      .when((p) => p.paymentType === "credit");
+  }
+
+  public binding(): void {
+    this.cart = this.api.getCart();
+    this.totalItems = this.api.getCartTotal();
+
+    this.calculateTotal();
+  }
+
+  public async submit(): Promise<void> {
+    const result = await this.validationController.validate();
+
+    if (result.valid) {
+      this.processing = true;
+
+      await sleep(1500); // wait 1.5 seconds before going to the server
+      const order: { orderId: number; success: boolean } =
+        await this.api.processOrder(1, this.details, this.cart);
+
+      if (order.success) {
+        this.router.load(`/order/${order.orderId}`);
+      }
+
+      this.processing = false;
     }
+  }
 
-    public binding(): void {
-        this.cart = this.api.getCart();
-        this.totalItems = this.api.getCartTotal();
+  private calculateTotal(): void {
+    this.total = this.cart
+      .reduce((runningTotal, product) => {
+        const total = parseInt(product.quantity) * product.price;
 
-        this.calculateTotal();
-    }
-
-    public async submit(): Promise<void> {
-        const result = await this.validationController.validate();
-    
-        if (result.valid) {
-            this.processing = true;
-    
-            await sleep(1500); // wait 1.5 seconds before going to the server
-            const order: { orderId: number; success: boolean } = await this.api.processOrder(1, this.details, this.cart);
-    
-            if (order.success) {
-                this.router.load(`/order/${order.orderId}`);
-            }
-    
-            this.processing = false;
-        }
-    }
-
-    private calculateTotal(): void {
-        this.total = this.cart.reduce((runningTotal, product) => {
-            const total = parseInt(product.quantity) * product.price;
-
-            return runningTotal + total;
-        }, 0).toFixed(2);
-    }
+        return runningTotal + total;
+      }, 0)
+      .toFixed(2);
+  }
 }
